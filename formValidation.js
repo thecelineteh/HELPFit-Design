51906f89fded928443a15b9f1af65402ed9098be
B1601563 <celineteh2011@gmail.com>
2017-10-05 03:58:13 +0800

    my js validation at update record

--- formValidation.js
+++ formValidation.js
@@ -22,6 +22,38 @@ function validateForm() {
       return false;
     }
     else{
-      alert("THANK YOU, Your session is successfully created !");
+      alert("Thank you, your session is successfully created !");
+    }
+}
+
+
+function updateForm() {
+    var date = document.forms["myForm"]["sessionDate"].value;
+    var time = document.forms["myForm"]["sessionTime"].value;
+    var fee = document.forms["myForm"]["sessionFee"].value;
+    var status = document.forms["myForm"]["sessionStatus"].value;
+    var classType = document.forms["myForm"]["sessionType"].value;
+    if (date === ""){
+      alert("Date cannot be blank.");
+      return false;
+    }
+    else if (time === ""){
+      alert("Time cannot be blank.");
+      return false;
+    }
+    else if (fee === ""){
+      alert("Fee cannot be blank.");
+      return false;
+    }
+    else if (status === "Choose status"){
+      alert("Status must be either Cancelled, Completed, or Available.");
+      return false;
+    }
+    else if (classType === "Choose class type"){
+      alert("Class type must be either Sport, Dance, or MMA.");
+      return false;
+    }
+    else{
+      alert("Session updated successfully !");
     }
 }
