e5bcd501a9125f9eef2d9557c16fb6631f848574
B1601563_CT <celineteh2011@gmail.com>
2017-10-20 22:49:51 +0800

    validationCheck
    updateForm() function not working

--- formValidation.js
+++ formValidation.js
@@ -4,21 +4,25 @@ function validateForm() {
     var title = document.forms["myForm"]["title"].value;
     var date = document.forms["myForm"]["sessionDate"].value;
     var time = document.forms["myForm"]["sessionTime"].value;
-    var fee = document.forms["myForm"]["fee"].value;
+    var fee = document.forms["myForm"]["sessionFee"].value;
     if (title === "") {
-        alert("Title cannot be blank !");
+        alert("Title cannot be blank.");
+        document.forms["myForm"]["title"].focus();
         return false;
     }
     else if (date === ""){
-      alert("Date cannot be blank ! ");
+      alert("Date cannot be blank.");
+      document.forms["myForm"]["sessionDate"].focus();
       return false;
     }
     else if (time === ""){
-      alert("Time cannot be blank ! ");
+      alert("Time cannot be blank.");
+      document.forms["myForm"]["sessionTime"].focus();
       return false;
     }
     else if (fee === ""){
-      alert("Fee cannot be blank ! ");
+      alert("Fee cannot be blank.");
+      document.forms["myForm"]["sessionFee"].focus();
       return false;
     }
     else{
@@ -28,29 +32,34 @@ function validateForm() {
 
 
 function updateForm() {
-    var date = document.forms["myForm"]["sessionDate"].value;
-    var time = document.forms["myForm"]["sessionTime"].value;
-    var fee = document.forms["myForm"]["sessionFee"].value;
-    var status = document.forms["myForm"]["sessionStatus"].value;
-    var classType = document.forms["myForm"]["sessionType"].value;
+    var date = document.forms["updateForm"]["sessionDate"].value;
+    var time = document.forms["updateForm"]["sessionTime"].value;
+    var fee = document.forms["updateForm"]["sessionFee"].value;
+    var status = document.forms["updateForm"]["sessionStatus"].value;
+    var classType = document.forms["updateForm"]["sessionType"].value;
     if (date === ""){
       alert("Date cannot be blank.");
+      document.forms["updateForm"]["sessionDate"].focus();
       return false;
     }
     else if (time === ""){
       alert("Time cannot be blank.");
+      document.forms["updateForm"]["sessionTime"].focus();
       return false;
     }
     else if (fee === ""){
       alert("Fee cannot be blank.");
+      document.forms["updateForm"]["sessionFee"].focus();
       return false;
     }
     else if (status === "Choose status"){
       alert("Status must be either Cancelled, Completed, or Available.");
+      document.forms["updateForm"]["sessionStatus"].focus();
       return false;
     }
     else if (classType === "Choose class type"){
       alert("Class type must be either Sport, Dance, or MMA.");
+      document.forms["updateForm"]["sessionType"].focus();
       return false;
     }
     else{
